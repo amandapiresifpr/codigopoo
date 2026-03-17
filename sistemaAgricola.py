fa4fab18375d08539b85f2a9b3291d75dd5fbe29
unknown <{email}>
2026-03-17 09:22:19 -0300

    modificacao final

--- sistemaAgricola.py
+++ sistemaAgricola.py
@@ -1,3 +1,14 @@
+def calc_desconto(precos, desconto):
+    for i in range(0, len(precos)):
+        valor = precos[i]
+        result = valor - (valor * (desconto/100))
+        precos[i] = result
+    return precos
+
+indiceProd = 0
+nomes = [""] * 10
+precos = []
+
 op = 0
 while(op != 4):
     print("""1 - Cadastrar Produto
@@ -5,13 +16,29 @@ while(op != 4):
     3 - Aplicar Desconto
     4 - Sair do Programa""")
     op = int(input("Digite um número de 1 a 4 para escolher uma opção: "))
+
     if(op == 1):
-        print("1 - Cadastrar Produto")
+        print("Cadastrar Produto")
+        nome = str(input("Digite o nome do produto"))
+        preco = float(input("Preço: R$ "))
+        nomes[indiceProd] = nome
+        indiceProd += 1
+        precos.append(preco)
+
     elif(op == 2):
-        print("2 - Listar Produtos")
+        print("Listar Produtos")
+        for i in range(0, len(precos)):
+            print(nomes[i], " - ", precos[i])
+
     elif(op == 3):
-        print("3 - Aplicar Desconto")
+        print("Aplicar Desconto")
+        desconto = int(input("Digite a porcentagem do desconto"))
+        precos = calc_desconto(precos, desconto)
+        for i in range(0, len(precos)):
+            print(nomes[i], "-", precos[i])
+
     elif(op == 4):
-        print("4 - Sair do Programa")
+        print("Sair do Programa")
+
     else:
         print("Opção inválida, digite novamente")
